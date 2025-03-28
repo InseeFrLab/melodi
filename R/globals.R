fad7952876fdab7bdc346b196573a3b3c7119a09
Cédric Bobinec <cedric.bobinec@insee.fr>
2025-03-28 14:52:32 +0100

    résolution notes

--- R/globals.R
+++ R/globals.R
@@ -5,6 +5,7 @@ globalVariables(
     "subtitle","description", "abstract", "temporal", "title_lang",
     "subtitle_lang", "description_lang", "abstract_lang",
     "temporal_startPeriod", "temporal_endPeriod",
+    "spatialTemporal", "spatialResolution",
     # get_all_data
     "COD_VAR", "LIB_VAR", "language", "accessURL", "filename",
     # get_data
