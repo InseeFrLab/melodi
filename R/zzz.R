b53eecd4674c91c0779b03b3e2359aea0bd40d60
ggj62i <cedric.bobinec@insee.fr>
2026-07-17 13:22:14 +0000

    retour arrière : semble faire bugger les tests

--- R/zzz.R
+++ R/zzz.R
@@ -1,23 +1,10 @@
 .onLoad <- function(libname, pkgname) {
-  op <- options()
-
-  op.rmelodi(
+  options(
     # Beginning of API URL (change only for developpement/internal use)
     rmelodi.base_url_api = "https://api.insee.fr/melodi",
     # Maximum maxResult allowed by Melodi API (change only if API improvement)
     rmelodi.max_result_api = 100000,
-    # Define user agent sent in headers with every requests
-    # (default : 'RMelodi/1.0.0')
-    rmelodi.req_user_agent = glue::glue(
-      "RMelodi/{utils::packageVersion('melodi')}"
-    )
-  )
-
-  # Restauration des options à la sortie de la fonction (même en cas d'erreur)
-  # Bonne pratique :
-  # Réinitialiser à leur état d'origine lorsque le package est détaché
-  on.exit(
-    options(old_options),
-    add = TRUE
+    # Define user agent sent in headers with every requests (default : 'RMelodi/1.0.0')
+    rmelodi.req_user_agent = glue::glue("RMelodi/{utils::packageVersion('melodi')}")
   )
 }
